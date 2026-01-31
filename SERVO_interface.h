9981ab366764585cea17b602b2b9524ab5bf2fd8
Omar-Saafan <126273426+Omar-Saafan@users.noreply.github.com>
2026-01-31 19:54:15 +0200

    Add files via upload

---
+++ SERVO_interface.h
@@ -0,0 +1,14 @@
+/*
+ * SERVO_interface.h
+ *
+ *  Created on: Jan 31, 2026
+ *      Author: omarm
+ */
+
+#ifndef SERVO_INTERFACE_H_
+#define SERVO_INTERFACE_H_
+
+void SERVO_voidInit();
+void SERVO_voidOn(f32 copy_f32Angle);
+
+#endif /* SERVO_INTERFACE_H_ */
---
+++ TIMER0_config.h
@@ -0,0 +1,21 @@
+/*
+ * TIMER0_config.h
+ *
+ *  Created on: Jan 31, 2026
+ *      Author: omarm
+ */
+
+#ifndef TIMER0_CONFIG_H_
+#define TIMER0_CONFIG_H_
+
+#define TIMER0_PRELOAD_VALUE                        113
+#define TIMER0_OVERFLOW_COUNTER                     977
+
+#define TIMER0_OUTPUT_COMPARE_VALUE                 249
+#define TIMER0_OUTPUT_COMPARE_COUNTER               1500 
+
+#define TIMER0_FAST_PWM_DUTY_CYCLE                  80
+
+#define TIMER0_MODE                  TIMER0_FAST_PWM_MODE
+
+#endif /* TIMER0_CONFIG_H_ */
